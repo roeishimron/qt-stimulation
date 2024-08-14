f182a14d44034ca2505f7500ab1ee937c3713e73
Roei Shimron <roei@mail.academy.ac.il>
2024-08-14 15:12:21 +0000

    stimulates at exact rate

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
 import sys
-from PySide6.QtWidgets import QApplication, QPushButton
-from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QPushButton, QWidget, QPushButton, QLabel, QVBoxLayout, QMainWindow
+from PySide6.QtCore import Slot, QSize, QTimer, Qt
+from PySide6.QtGui import QPixmap, QScreen
+from stims import generate_sin
+from typing import List, Iterable
+from itertools import cycle
 
-@Slot()
-def say_hello():
- print("Button clicked, Hello!")
+
+class ImageDecider:
+    pixmaps: Iterable[QPixmap]
+    image: QLabel
+
+    def __init__(self, pixmaps: List[QPixmap], image: QLabel):
+        self.pixmaps = cycle(pixmaps)
+
+        self.image = image
+
+        self.next()
+
+    @Slot()
+    def next(self):
+        self.image.setPixmap(next(self.pixmaps))
+
+
+class MainWindow(QMainWindow):
+    decider: ImageDecider
+    screen: QScreen
+    timer: QTimer
+    display: QLabel
+
+    def __init__(self, screen: QScreen):
+        super().__init__()
+
+        self.screen = screen
+        screen_height = screen.size().height()
+
+        self.display = QLabel(self)
+        self.display.setAlignment(Qt.AlignCenter)
+        self.display.setWordWrap(True)
+        self.display.setMargin(100)
+        self.display.setStyleSheet(
+            '''
+                            background: rgb(127, 127, 127);
+                            color: #bbb;
+                            font-size: 28pt;
+                    '''
+        )
+        self.setCentralWidget(self.display)
+
+        self.decider = ImageDecider([generate_sin(int(screen_height*3/4), 5),
+                                     generate_sin(int(screen_height*3/4), 50)], self.display)
+        timer = QTimer(self)
+        timer.setTimerType(Qt.TimerType.PreciseTimer)
+        timer.setInterval(1000/6)
+        timer.timeout.connect(self.decider.next)
+        timer.start()
+
 
 # Create the Qt Application
 app = QApplication(sys.argv)
-# Create a button, connect it and show it
-button = QPushButton("Click me")
-button.clicked.connect(say_hello)
-button.show()
+
+main_window = MainWindow(app.primaryScreen())
+main_window.showFullScreen()
+
 # Run the main Qt loop
 app.exec()
---
+++ stims.py
@@ -0,0 +1,20 @@
+from numpy import array, sin, pi, linspace, uint8, tile
+from PySide6.QtGui import QPixmap, QImage
+
+
+def generate_sin(figure_size, frequency=1, offset=0, contrast=1) -> QPixmap:
+
+    assert figure_size >= 2*frequency
+
+    sin_flips = (frequency*figure_size)
+    virtual_size = sin_flips * 2 * pi
+
+    x_range = linspace(offset, virtual_size + offset, figure_size)
+    sinsusoid = sin(x_range)
+
+    mormalized_to_pixels = (((sinsusoid * 255) * contrast + 255)/2)
+
+    image_line = mormalized_to_pixels.astype(uint8)
+    raw_array = tile(image_line, (figure_size, 1))
+
+    return QPixmap.fromImage(QImage(raw_array, figure_size, figure_size, figure_size, QImage.Format_Grayscale8))
