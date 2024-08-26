d7d299c98214358d8b7bf6fa5e625cabfecbe0d9
Roei Shimron <roei@mail.academy.ac.il>
2024-08-26 07:49:32 +0000

    move display logic into the stimuli decider

--- main.py
+++ main.py
@@ -4,36 +4,32 @@ from PySide6.QtCore import Slot, Qt
 from PySide6.QtGui import QScreen, QKeyEvent
 from stims import generate_sin
 from soft_serial import SoftSerial
-from stimuli_decider import ImageDecider
+from stimuli_decider import StimuliDecider
 from timers import Timers
 
 
 class MainWindow(QMainWindow):
-    decider: ImageDecider
+    decider: StimuliDecider
     screen: QScreen
     timers: Timers
-    display: QLabel
     event_trigger: SoftSerial
 
     @Slot()
     def frame_change(self):
-        self.decider.next()
+        self.decider.next_stim()
         self.event_trigger.write_int(1)
 
     @Slot()
     def trial_end(self):
         self.timers.start_break()
         self.event_trigger.write_int(2)
-        self.display_break()
+        self.decider.display_break()
         self.keyReleaseEvent = self.key_released_at_break
 
     def break_end(self):
         self.keyReleaseEvent = self.key_released_default
         self.timers.start_trial()
         self.event_trigger.write_int(3)
-
-    def display_break(self):
-        self.display.setText("This is a break.\nPress any key to continue")
 
     def __init__(self, screen: QScreen, event_trigger: SoftSerial):
         super().__init__()
@@ -43,21 +39,10 @@ class MainWindow(QMainWindow):
         self.screen = screen
         screen_height = screen.size().height()
 
-        self.display = QLabel(self)
-        self.display.setAlignment(Qt.AlignCenter)
-        self.display.setWordWrap(True)
-        self.display.setMargin(100)
-        self.display.setStyleSheet(
-            '''
-                            background: rgb(127, 127, 127);
-                            color: #bbb;
-                            font-size: 28pt;
-                    '''
-        )
-        self.setCentralWidget(self.display)
+        self.decider = StimuliDecider([generate_sin(int(screen_height*3/4), 5),
+                                       generate_sin(int(screen_height*3/4), 50)], QLabel(self))
 
-        self.decider = ImageDecider([generate_sin(int(screen_height*3/4), 5),
-                                     generate_sin(int(screen_height*3/4), 50)], self.display)
+        self.setCentralWidget(self.decider.display)
 
         self.timers = Timers(self.frame_change, self.trial_end)
         self.break_end()
--- stimuli_decider.py
+++ stimuli_decider.py
@@ -2,18 +2,33 @@ from PySide6.QtWidgets import QLabel
 from PySide6.QtGui import QPixmap
 from typing import List, Iterable
 from itertools import cycle
+from PySide6.QtCore import Qt
 
 
-class ImageDecider:
+class StimuliDecider:
     pixmaps: Iterable[QPixmap]
-    image: QLabel
+    display: QLabel
 
-    def __init__(self, pixmaps: List[QPixmap], image: QLabel):
+    def __init__(self, pixmaps: List[QPixmap], display: QLabel):
+
+        self.display = display
+        self.display.setAlignment(Qt.AlignCenter)
+        self.display.setWordWrap(True)
+        self.display.setMargin(100)
+        self.display.setStyleSheet(
+            '''
+                            background: rgb(127, 127, 127);
+                            color: #bbb;
+                            font-size: 28pt;
+                    '''
+        )
+
         self.pixmaps = cycle(pixmaps)
 
-        self.image = image
+        self.next_stim()
 
-        self.next()
+    def next_stim(self):
+        self.display.setPixmap(next(self.pixmaps))
 
-    def next(self):
-        self.image.setPixmap(next(self.pixmaps))
+    def display_break(self):
+        self.display.setText("This is a break.\nPress any key to continue")
