374773f88158b5aeae8bcc1d274ec06990ca8260
Roei Shimron <roei@mail.academy.ac.il>
2024-09-03 07:59:58 +0000

    split into main and experiment

--- main.py
+++ main.py
@@ -1,90 +1,19 @@
 import sys
-from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
-from PySide6.QtCore import Slot, Qt, QCoreApplication
-from PySide6.QtGui import QScreen, QKeyEvent
+from PySide6.QtWidgets import QApplication
 from stims import generate_sin, generate_grey
-from soft_serial import SoftSerial, Codes
-from stimuli_decider import Animator
+from soft_serial import SoftSerial
 
-
-FREQUENCY_MS = 1000/6
-REPETITIONS_PER_TRIAL = 30 * 1000 / FREQUENCY_MS  # 30s
-
-
-class MainWindow(QMainWindow):
-    animator: Animator
-    screen: QScreen
-    event_trigger: SoftSerial
-    background: QLabel
-
-    @Slot()
-    def frame_change(self):
-        self.event_trigger.write_int(Codes.FrameChange)
-
-    @Slot()
-    def trial_end(self):
-        self.animator.stop()
-        self.event_trigger.write_int(Codes.TrialEnd)
-        self.animator.display_break()
-        self.keyReleaseEvent = self.key_released_at_break
-
-    def trial_start(self):
-        self.keyReleaseEvent = self.key_released_default
-        self.animator.start()
-        self.event_trigger.write_int(Codes.BreakEnd)
-
-    def __init__(self, screen: QScreen, event_trigger: SoftSerial):
-        super().__init__()
-
-        self.event_trigger = event_trigger
-
-        self.screen = screen
-        screen_height = screen.size().height()
-
-        self.background = QLabel(self)
-        self.background.setAlignment(Qt.AlignCenter)
-        self.background.setWordWrap(True)
-        self.background.setMargin(100)
-        self.background.setStyleSheet(
-            '''
-                            background: rgb(127, 127, 127);
-                            color: #bbb;
-                            font-size: 28pt;
-                    '''
-        )
-        self.background.setPixmap(generate_grey(int(screen_height*3/4)))
-
-        self.animator = Animator([generate_sin(int(screen_height*3/4), 5),
-                                  generate_sin(int(screen_height*3/4), 50)], QLabel(self.background),
-                                 FREQUENCY_MS, REPETITIONS_PER_TRIAL, self.trial_end, self.frame_change)
-
-        self.setCentralWidget(self.background)
-
-        self.trial_start()
-
-    def quit(self):
-        self.event_trigger.write_int(Codes.Quit)
-        print("Quitting")
-        QCoreApplication.quit()
-
-    def key_released_at_break(self, event: QKeyEvent):
-        if event.key() == Qt.Key.Key_Q:
-            self.quit()
-        else:
-            # seems like `quit()` returns
-            self.trial_start()
-
-    def key_released_default(self, event: QKeyEvent):
-        if event.key() == Qt.Key.Key_B:
-            self.trial_end()
-        else:
-            print("Key pressed, doing nothing")
+from viewing_experiment import ViewExperiment
 
 
 # Create the Qt Application
 app = QApplication(sys.argv)
 
-main_window = MainWindow(app.primaryScreen(), SoftSerial())
+screen_height = app.primaryScreen().geometry().height()
+
+main_window = ViewExperiment(screen_height, [generate_sin(int(screen_height*3/4), 5),
+                                             generate_sin(int(screen_height*3/4), 50)],
+                             SoftSerial())
 main_window.show()
 
 # Run the main Qt loop
---
+++ viewing_experiment.py
@@ -0,0 +1,77 @@
+import sys
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtCore import Slot, Qt, QCoreApplication
+from PySide6.QtGui import QScreen, QKeyEvent
+from stims import generate_sin, generate_grey
+from soft_serial import SoftSerial, Codes
+from stimuli_decider import Animator
+from typing import List
+
+FREQUENCY_MS = 1000/6
+REPETITIONS_PER_TRIAL = 30 * 1000 / FREQUENCY_MS  # 30s
+
+
+class ViewExperiment(QMainWindow):
+    animator: Animator
+    screen: QScreen
+    event_trigger: SoftSerial
+    background: QLabel
+
+    @Slot()
+    def frame_change(self):
+        self.event_trigger.write_int(Codes.FrameChange)
+
+    @Slot()
+    def trial_end(self):
+        self.animator.stop()
+        self.event_trigger.write_int(Codes.TrialEnd)
+        self.animator.display_break()
+        self.keyReleaseEvent = self.key_released_at_break
+
+    def trial_start(self):
+        self.keyReleaseEvent = self.key_released_default
+        self.animator.start()
+        self.event_trigger.write_int(Codes.BreakEnd)
+
+    def __init__(self, screen_height:int, stimulations: List, event_trigger: SoftSerial):
+        super().__init__()
+
+        self.event_trigger = event_trigger
+
+        self.background = QLabel(self)
+        self.background.setAlignment(Qt.AlignCenter)
+        self.background.setWordWrap(True)
+        self.background.setMargin(100)
+        self.background.setStyleSheet(
+            '''
+                            background: rgb(127, 127, 127);
+                            color: #bbb;
+                            font-size: 28pt;
+                    '''
+        )
+        self.background.setPixmap(generate_grey(int(screen_height*3/4)))
+
+        self.animator = Animator(stimulations, QLabel(self.background),
+                                 FREQUENCY_MS, REPETITIONS_PER_TRIAL, self.trial_end, self.frame_change)
+
+        self.setCentralWidget(self.background)
+
+        self.trial_start()
+
+    def quit(self):
+        self.event_trigger.write_int(Codes.Quit)
+        print("Quitting")
+        QCoreApplication.quit()
+
+    def key_released_at_break(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_Q:
+            self.quit()
+        else:
+            # seems like `quit()` returns
+            self.trial_start()
+
+    def key_released_default(self, event: QKeyEvent):
+        if event.key() == Qt.Key.Key_B:
+            self.trial_end()
+        else:
+            print("Key pressed, doing nothing")
