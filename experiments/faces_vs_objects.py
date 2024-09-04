055b967958ac1127ba7a36008c6ccaaa45726c7b
Roei Shimron <roei@mail.academy.ac.il>
2024-09-04 12:30:48 +0000

    faces with random inflation

--- experiments/faces_vs_objects.py
+++ experiments/faces_vs_objects.py
@@ -4,9 +4,9 @@ from PySide6.QtWidgets import QApplication
 from stims import generate_sin
 from soft_serial import SoftSerial
 from animator import OddballStimuli
-from itertools import cycle
+from itertools import cycle, chain
 from viewing_experiment import ViewExperiment
-from typing import List, Generator, Any
+from typing import List, Generator, Any, Iterable
 from random import shuffle
 
 import os
@@ -22,6 +22,17 @@ def read_images_into_pixmaps(path: str, screen_height: int) -> Generator[QPixmap
             (screen_height*3/4)).convertedTo(QImage.Format.Format_Grayscale8)
         yield QPixmap.fromImage(scaled)
 
+
+def inflate_randomley(source: List[Any], factor: int) -> Iterable[Any]:
+    def inflate() -> Generator[List[Any], None, None]:
+        for _ in range(factor):
+            current = list(source)
+            shuffle(current)
+            yield current
+
+    return chain.from_iterable(inflate())
+
+
 def run():
     # Create the Qt Application
     app = QApplication(sys.argv)
@@ -33,7 +44,8 @@ def run():
     objects = list(read_images_into_pixmaps(
         "experiments/faces-data/objects", screen_height))
 
-    stimuli = OddballStimuli(cycle(faces), cycle(objects), 3)
+    stimuli = OddballStimuli(cycle(list(inflate_randomley(faces, 100))),
+                             cycle(list(inflate_randomley(objects, 100))), 3)
 
     main_window = ViewExperiment(stimuli, SoftSerial())
     main_window.showFullScreen()
