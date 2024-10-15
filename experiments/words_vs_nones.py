7a9a6cdcaa97e4fabbfb1fe7d8e8312d0639cb9a
Roei Shimron <roei@mail.academy.ac.il>
2024-10-15 17:47:36 +0000

    add words experiment

---
+++ experiments/words_vs_nones.py
@@ -0,0 +1,26 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from stims import inflate_randomley
+from soft_serial import SoftSerial
+from animator import OddballStimuli, AppliableText
+from itertools import cycle
+from viewing_experiment import ViewExperiment
+
+
+def run():
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+
+    screen_height = app.primaryScreen().geometry().height()
+
+    size = int(screen_height*3/4)
+
+    stimuli = map(AppliableText, inflate_randomley(["לא", "את", "כל", "אין", "היה", "אמר", "כאן", "שלום", "חבר"], 100)) 
+    oddballs = map(AppliableText, inflate_randomley(["بجث", "زحخ", "منغ", "صضقس", "منسش", "ذوطظ"], 100))
+
+    main_window = ViewExperiment(OddballStimuli(
+        size, cycle(stimuli), cycle(oddballs), 2), SoftSerial(), 2.5, trial_duration=180)
+    main_window.show()
+
+    # Run the main Qt loop
+    app.exec()
