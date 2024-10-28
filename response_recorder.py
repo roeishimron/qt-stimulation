d409ecdd224d78bb7b9415a821f32ac2aef783c5
Roei Shimron <roei@mail.academy.ac.il>
2024-10-28 07:22:58 +0000

    taking false-presses into account

--- response_recorder.py
+++ response_recorder.py
@@ -11,16 +11,19 @@ class Stimuli:
 class ResponseRecorder:
     stimuli_times: List[Stimuli]
     resopnse_delays: List[int]
+    false_pressing: int
 
     def __init__(self):
         self.stimuli_times = [Stimuli(True, 0)]
         self.resopnse_delays = []
+        self.false_pressing = 0
 
     def record_stimuli_show(self):
         self.stimuli_times.append(Stimuli(False, time_ns()))
 
     def record_response(self):
         if self.stimuli_times[-1].responded:
+            self.false_pressing += 1
             return
 
         diff = time_ns() - self.stimuli_times[-1].showed_at
@@ -28,7 +31,9 @@ class ResponseRecorder:
         self.resopnse_delays.append(diff)
         print(f"got response after {diff} ns (or {diff/10**6} ms)")
 
+    # adding the false-pressing to the amount of stimuli (so it counts like an unreported stimulus)
     def success_rate(self):
         if len(self.stimuli_times) == 1:
-            return 1
-        return len([w for w in self.resopnse_delays if w < 2 * 10**9]) / (len(self.stimuli_times) - 1)
+            return int(self.false_pressing == 0) # nothing with nothing is 1, otherwise 0
+        
+        return len([w for w in self.resopnse_delays if w < 2 * 10**9]) / (len(self.stimuli_times) - 1 + self.false_pressing)
