189e591a40b5adcfb00815040b244461fe121bd5
Roei Shimron <roei@mail.academy.ac.il>
2024-11-03 08:42:25 +0000

    set default font size to 50

--- animator.py
+++ animator.py
@@ -31,7 +31,7 @@ class AppliableText(Appliable):
     font_family: str
     font_style: QFont.Style
 
-    def __init__(self, text: str, font_size:int=28, color:str=DEFAULT_COLOR, 
+    def __init__(self, text: str, font_size:int=50, color:str=DEFAULT_COLOR, 
                  font_family: str = DEFAULT_FONT, font_style: QFont.Style=QFont.Style.StyleNormal):
         self.text = text
         self.font_size = font_size
