551ccb4aaf92c737f0ddab71c1ee36f8864ac3ba
Roei Shimron <roei@mail.academy.ac.il>
2024-12-11 16:06:11 +0000

    re-implement modulating sin

--- experiments/modulating_sin.py
+++ experiments/modulating_sin.py
@@ -1,25 +1,54 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from stims import generate_sin
+from PySide6.QtCore import Qt
+from typing import List, Iterable
+from stims import generate_increasing_durations, generate_sin, generate_grey
+from response_recorder import ResponseRecorder
 from soft_serial import SoftSerial
-from animator import OddballStimuli
+from animator import OddballStimuli, AppliableText, OnShowCaller, Appliable
 from itertools import cycle
 from viewing_experiment import ViewExperiment
+from random import choices, random, choice
+from experiments.words import COMMON_HEBREW_WORDS, into_arabic
+from copy import deepcopy
+from numpy import pi
+
+
+def create_on_show_caller(t: AppliableText) -> OnShowCaller:
+    return OnShowCaller(t, lambda: None)
+
+
+def create_random_duplications(words: List[OnShowCaller], recorder: ResponseRecorder) -> List[OnShowCaller]:
+    n = len(words)
+    indices = choices(range(1, n), k=int((n-1)/20))
+    for i in indices:
+        words[i] = deepcopy(words[i-1])
+        words[i].on_show = recorder.record_stimuli_show
+    return words
 
 
 def run():
     # Create the Qt Application
     app = QApplication(sys.argv)
 
+    recorder = ResponseRecorder()
+
     screen_height = app.primaryScreen().geometry().height()
 
     size = int(screen_height*3/4)
 
-    stimuli = [generate_sin(size, 5), generate_sin(size, 5, 180)]
+    base = [generate_sin(size, 10, random()*pi,
+                         horizontal=choice([True, False]), step=True) for _ in range(100)]
+    oddballs = [generate_grey(size)]
 
-    main_window = ViewExperiment.new_with_constant_frequency(OddballStimuli(
-        size, cycle(stimuli)), SoftSerial(), 5.88, trial_duration=180, fixation="+")
+    main_window = ViewExperiment.new(OddballStimuli(
+        size, cycle(oddballs), cycle(base), 2),
+        SoftSerial(),
+        generate_increasing_durations(10),
+        use_step=True, fixation="+",
+        on_runtime_keypress=lambda e: recorder.record_response() if e.key() == Qt.Key.Key_Space else print("pass"))
     main_window.show()
 
     # Run the main Qt loop
     app.exec()
+    print(f"answered with success rate of {recorder.success_rate()}")
