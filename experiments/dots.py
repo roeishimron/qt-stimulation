41a7ee5f726d572e69038f0907727680ca5c1c6d
Roei Shimron <roei@mail.academy.ac.il>
2024-12-29 10:53:48 +0000

    updated dots to work

--- experiments/dots.py
+++ experiments/dots.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from stims import fill_with_dots, inflate_randomley
+from stims import fill_with_dots, inflate_randomley, create_gabor_values, array_into_pixmap
 from soft_serial import SoftSerial
 from animator import OddballStimuli
 from itertools import cycle
@@ -15,26 +15,28 @@ def run():
 
     screen_height = app.primaryScreen().geometry().height()
 
-    size = screen_height*3/4
+    height = int(screen_height*2/3)
 
-    BASE_SIZE = 20 # keeping constant as a result of the following article
-    BASE_AMOUNT = 35
-
-    AVERAGE_AREA = int(pi * square(BASE_SIZE) * BASE_AMOUNT)
-
-    # match average area with oddball.
     # According to https://www.pnas.org/doi/epdf/10.1073/pnas.1917849117 it's more important than controlling the figure size.
-    ODDBALL_AMOUNT = 6
-    ODDBALL_AVERAGE_RADIUS = int(sqrt(AVERAGE_AREA / ODDBALL_AMOUNT / pi))
+    GABOR_SIZE = 100
+    GABOR_FREQ = 2
+    RADIAL_EASING = 120
 
-    base = (fill_with_dots(int(size), BASE_AMOUNT, BASE_SIZE) for _ in range(120))
-    oddball = (fill_with_dots(int(size), ODDBALL_AMOUNT, ODDBALL_AVERAGE_RADIUS) for _ in range(20))
-    
+    base = (array_into_pixmap(fill_with_dots(int(height), array([
+        create_gabor_values(GABOR_SIZE, GABOR_FREQ, horizontal=True,
+                            raidal_easing=RADIAL_EASING) for _ in range(int(16))
+    ]))) for _ in range(20))
+
+    oddball = (array_into_pixmap(fill_with_dots(int(height), array([
+        create_gabor_values(GABOR_SIZE, GABOR_FREQ, horizontal=True,
+                            raidal_easing=RADIAL_EASING) for _ in range(int(8))
+    ]))) for _ in range(20))
+
     main_window = ViewExperiment.new_with_constant_frequency(
-        OddballStimuli(size, cycle(inflate_randomley(oddball, 10)),
-                       cycle(inflate_randomley(base, 10)), 5), SoftSerial(), 12, True,
+        OddballStimuli(height, cycle(inflate_randomley(oddball, 10)),
+                       cycle(inflate_randomley(base, 10)), 5), SoftSerial(), 5, True,
         fixation="+", trial_duration=45)
-    main_window.show()
+    main_window.experiment.show()
 
     # Run the main Qt loop
     app.exec()
