302ff352f63d5d22b88f997280ebb9f103bbc725
roei shimron <roeishimron@gmail.com>
2025-01-02 08:44:12 +0000

    update and move dots

--- experiments/staircase_two_stims/dots.py
+++ experiments/staircase_two_stims/dots.py
@@ -1,28 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import Qt
-from typing import List, Iterable
-from stims import generate_noise, generate_solid_color, fill_with_dots, create_gabor_values
-from response_recorder import ResponseRecorder
+from stims import generate_noise, fill_with_dots, create_gabor_values
 from soft_serial import SoftSerial
-from animator import OddballStimuli, AppliableText, OnShowCaller, Appliable
 from itertools import cycle
-from staircase_experiment import StaircaseExperiment, TimedChoiceGenerator, ExperimentState
-from random import choices, random, choice
-from experiments.words import COMMON_HEBREW_WORDS, into_arabic
-from copy import deepcopy
-from numpy import pi, array
-from json import loads
-from matplotlib.pyplot import plot, show
-
-
-def log_into_graph():
-    sorted_states = list(map(lambda l: ExperimentState(
-        **loads(l)),  open("results.txt").read().splitlines()))
-    xs = list(map(lambda s: s.trial_no, sorted_states))
-    ys = list(map(lambda s: 33-s.difficulty, sorted_states))
-    plot(xs, ys)
-    show(block=True)
+from staircase_experiment import StaircaseExperiment, TimedChoiceGenerator
+from random import random
+from numpy import pi
+from numpy.typing import NDArray
 
 
 def run():
@@ -37,7 +21,7 @@ def run():
     GABOR_SIZE = 100
     RADIAL_EASING = 1000
 
-    targets = (fill_with_dots(int(height), [
+    stims = (fill_with_dots(int(height), [
         create_gabor_values(GABOR_SIZE, GABOR_FREQ, rotation=random()*pi/2,
                             raidal_easing=RADIAL_EASING) for _ in range(int(16))
     ])
@@ -49,10 +33,10 @@ def run():
     ])
         for _ in range(20))
 
-    mask = generate_noise(width, height, 18)
+    mask = generate_noise(width, height, 32)
 
     generator = TimedChoiceGenerator(
-        (height, width), cycle(targets), cycle(nons), cycle([mask]))
+        (height, width), cycle(stims), cycle(nons), cycle([mask]))
 
     main_window = StaircaseExperiment.new(height, generator,
                                           SoftSerial(),
@@ -60,4 +44,4 @@ def run():
     main_window.experiment.show()
     # Run the main Qt loop
     app.exec()
-    log_into_graph()
+    main_window.log_into_graph()
