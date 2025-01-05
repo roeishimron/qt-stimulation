f733ca95a23e6d77ca5eecf26f00869851964dac
Roei Shimron <roei@mail.academy.ac.il>
2025-01-05 16:57:41 +0000

    supporting side comperison with nework instead of random

--- experiments/staircase_two_stims/side.py
+++ experiments/staircase_two_stims/side.py
@@ -1,11 +1,11 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from stims import generate_noise, fill_with_dots, create_gabor_values, generate_grey
+from stims import Dot, generate_noise, fill_with_dots, create_gabor_values, generate_grey
 from soft_serial import SoftSerial
 from itertools import cycle
 from staircase_experiment import ConstantTimeChoiceGenerator, StaircaseExperiment, TimedChoiceGenerator
-from random import  random
-from numpy import pi
+from random import shuffle
+from numpy import linspace, array
 
 
 def run():
@@ -19,26 +19,31 @@ def run():
     GABOR_FREQ = 2
     GABOR_SIZE = 100
     RADIAL_EASING = 1000
-    AMOUNT_OF_TARGETS = 20
     STIM_DURATION = 200
 
-    targets = (fill_with_dots(int(height), [
-        create_gabor_values(GABOR_SIZE, GABOR_FREQ, rotation=random()*pi/2,
-                            raidal_easing=RADIAL_EASING)])
-        for _ in range(AMOUNT_OF_TARGETS))
+    position_xs = list(linspace(GABOR_SIZE/2, height-GABOR_SIZE/2, int(height/GABOR_SIZE-1)))
+    positions = [(x, y) for x in position_xs for y in position_xs]
+    shuffle(positions)
+
+    with open("positions.txt", "w") as f:
+        f.write(",".join(f"({x},{y})" for (x,y) in positions))
     
-    nons = [create_gabor_values(int(height), 0)] # this is grey
- 
+    dots = [Dot(GABOR_SIZE/2, array(p),
+                create_gabor_values(GABOR_SIZE, GABOR_FREQ, raidal_easing=RADIAL_EASING))
+            for p in positions]
+
+    targets = [fill_with_dots(int(height), [], [d]) for d in dots]
+    nons = [(create_gabor_values(int(height), 0))]  # this is grey
     mask = (generate_noise(width, height, 24) for _ in range(20))
 
-    generator = ConstantTimeChoiceGenerator((height, width), 
-                                            cycle(targets), cycle(nons), cycle(mask), 
+    generator = ConstantTimeChoiceGenerator((height, width),
+                                            cycle(targets), cycle(nons), cycle(mask),
                                             STIM_DURATION)
 
     main_window = StaircaseExperiment.new(height, generator,
-        SoftSerial(),
-        use_step=True, fixation="+", upper_limit=120)
-    
+                                          SoftSerial(),
+                                          use_step=True, fixation="+", upper_limit=len(dots)*2)
+
     main_window.show()
     # Run the main Qt loop
     app.exec()
