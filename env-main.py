322fa86c3aceb015103d91de633c144654cfaf6a
Roei Shimron <roei@mail.academy.ac.il>
2025-01-22 11:56:40 +0000

    add env-run input support

--- env-main.py
+++ env-main.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import experiments
 import experiments.staircase_two_stims
 import experiments.staircase_two_stims.dots
@@ -9,16 +9,23 @@ import experiments.staircase_two_stims.side
 import experiments.staircase_two_stims.side.demo
 import experiments.staircase_two_stims.side.side
 
-name = os.environ["name"]
-experiment = os.environ["experiment"]
+print("Subject Name:")
+name = input()
+print("Experiment Name:")
+experiment = input()
+
+output_folder = f"./{name}_results"
+Path(output_folder).mkdir(exist_ok=True)
 
 if experiment == "side":
-    experiments.staircase_two_stims.side.side.run()
+    experiments.staircase_two_stims.side.side.run(output_folder)
 elif experiment == "side_demo":
-    experiments.staircase_two_stims.side.demo.run()
+    experiments.staircase_two_stims.side.demo.run(output_folder)
 elif experiment == "numerosity_SOA":
-    experiments.staircase_two_stims.dots.dots.run()
+    experiments.staircase_two_stims.dots.dots.run(output_folder)
 elif experiment == "numerosity_demo":
-    experiments.staircase_two_stims.dots.dots_demo.run()
+    experiments.staircase_two_stims.dots.dots_demo.run(output_folder)
 elif experiment == "numerosity_ratio":
-    experiments.staircase_two_stims.dots_ratio.run()
+    experiments.staircase_two_stims.dots_ratio.run(output_folder)
+else:
+    print("No such experiment!")
