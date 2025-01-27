e54e4f996799a8cb13bb77bc8373e09bcb6e4db1
Roei Shimron <roei@mail.academy.ac.il>
2025-01-27 08:24:18 +0000

    format stims

--- stims.py
+++ stims.py
@@ -23,6 +23,7 @@ def gaussian(size: int, sigma):
 
     return exp(-(x**2 + y**2)/sigma)
 
+
 def _create_rotated_sin_frame(figure_size, frequency, offset, contrast, rotation) -> NDArray:
     sin_flips = frequency
     virtual_size = sin_flips * 2 * pi
@@ -32,11 +33,13 @@ def _create_rotated_sin_frame(figure_size, frequency, offset, contrast, rotation
     orthogonal_values = zeros((figure_size, figure_size))
     for x in range(int(-figure_size/2), int(figure_size/2)):
         for y in range(int(-figure_size/2), int(figure_size/2)):
-            orthogonal_values[int(x+figure_size/2),int(y+figure_size/2)] = sum(array([x,y]) * new_x_axis) + offset
-    
+            orthogonal_values[int(x+figure_size/2), int(y+figure_size/2)
+                              ] = sum(array([x, y]) * new_x_axis) + offset
+
     # normalize so that the max length arrives at the right amount of flips
     orthogonal_values = orthogonal_values * virtual_size / figure_size
     return sin(orthogonal_values)*contrast
+
 
 def _create_unrotated_sin_frame(figure_size, frequency, offset, contrast, horizontal: bool) -> NDArray:
     sin_flips = (frequency*figure_size)
@@ -52,14 +55,18 @@ def _create_unrotated_sin_frame(figure_size, frequency, offset, contrast, horizo
     return frame
 
 # currently, the frequency is NOT accurate due to the rotation support. It IS accurate for 0,90deg
-def create_gabor_values(figure_size, frequency=1, offset=0, contrast=1, 
+
+
+def create_gabor_values(figure_size, frequency=1, offset=0, contrast=1,
                         step=False, raidal_easing=inf, rotation=0) -> NDArray:
     assert figure_size >= 2*frequency
     frame = None
     if rotation % (pi/2) == 0:
-        frame = _create_unrotated_sin_frame(figure_size, frequency, offset, contrast, rotation % pi/2 == 0)
+        frame = _create_unrotated_sin_frame(
+            figure_size, frequency, offset, contrast, rotation % pi/2 == 0)
     else:
-        frame = _create_rotated_sin_frame(figure_size, frequency, offset, contrast, rotation)
+        frame = _create_rotated_sin_frame(
+            figure_size, frequency, offset, contrast, rotation)
 
     if step:
         frame = (frame > 0)*2-1
@@ -75,6 +82,8 @@ def generate_sin(figure_size, frequency=1, offset=0, contrast=1, step=False, rai
             figure_size, frequency, offset, contrast, step, raidal_easing, rotation))
 
 # assuming array of values between -1 and 1
+
+
 def array_into_pixmap(arr: NDArray) -> AppliablePixmap:
     mormalized_to_pixels = (((arr * 255) + 255)/2)
     mormalized_to_pixels = array(mormalized_to_pixels, dtype=uint8)
@@ -86,7 +95,9 @@ def generate_grey(figure_size: int) -> AppliablePixmap:
     return generate_sin(figure_size, 0, 0, 0)
 
 # left and right "pixel" values are between -1 and 1
-def place_in_figure(figure_size: Tuple[int,int], left: NDArray, right: NDArray) -> AppliablePixmap:
+
+
+def place_in_figure(figure_size: Tuple[int, int], left: NDArray, right: NDArray) -> AppliablePixmap:
     assert left.shape[1] + right.shape[1] <= figure_size[1]
     assert max(left.shape[0], right.shape[0]) <= figure_size[0]
 
@@ -98,8 +109,9 @@ def place_in_figure(figure_size: Tuple[int,int], left: NDArray, right: NDArray) 
     right_center = array([figure_size[0]/2, figure_size[1]/4*3], dtype=int)
     grid[right_center[0] - int(right.shape[0]/2):right_center[0] + int(right.shape[0]/2),
          right_center[1] - int(right.shape[1]/2):right_center[1] + int(right.shape[1]/2)] = right
-    
+
     return array_into_pixmap(grid)
+
 
 def generate_solid_color(figure_size: int, h: int, s: int = 255, v: int = 255):
     image = QImage(figure_size, figure_size, QImage.Format.Format_RGB16)
@@ -109,20 +121,20 @@ def generate_solid_color(figure_size: int, h: int, s: int = 255, v: int = 255):
     return AppliablePixmap(QPixmap.fromImage(image))
 
 
-def generate_noise(width: int, height: int, kernel_size: int=1) -> AppliablePixmap:
+def generate_noise(width: int, height: int, kernel_size: int = 1) -> AppliablePixmap:
     assert width % kernel_size == 0 and height % kernel_size == 0
     virtual_height = int(height / kernel_size)
     virtual_width = int(width / kernel_size)
 
     bools = rand(virtual_height, virtual_width) > 0.5
-    
+
     screen = zeros((height, width))
 
     for row in range(virtual_height):
         for col in range(virtual_width):
-            screen[row*kernel_size:(row+1)*kernel_size, col*kernel_size:(col+1)*kernel_size] = bools[row, col]
+            screen[row*kernel_size:(row+1)*kernel_size, col *
+                   kernel_size:(col+1)*kernel_size] = bools[row, col]
 
-    
     return array_into_pixmap(screen * 2 - 1)
 
 
@@ -171,9 +183,10 @@ def fill_with_white_dots(figure_size: int, amount_of_dots: int, dot_size: int) -
 
 
 def rec_at(position: int, width: int, height: int) -> Tuple[float64, Tuple[int, int]]:
-    vecs = [array([0,height]), array([width,0]), array([0,-height]), array([-width,0])]
+    vecs = [array([0, height]), array([width, 0]),
+            array([0, -height]), array([-width, 0])]
     assert position <= sum(abs(array(vecs).flatten()))
-    current_position = (0,0)
+    current_position = (0, 0)
     for v in vecs:
         length = sum(abs(v))
         if position - length < 0:
@@ -181,38 +194,43 @@ def rec_at(position: int, width: int, height: int) -> Tuple[float64, Tuple[int, 
             if v[0] == 0:
                 return (pi/2, (exact[0], exact[1]))
             return (0, (exact[0], exact[1]))
-            
+
         position -= length
         current_position += v
 
 
-def circle_at(center: Tuple[int,int], radius: int, angle_from_0: float64) -> Tuple[float64, Tuple[int, int]]:
-    
-    coordinates = array(array(center) + array([cos(angle_from_0), sin(angle_from_0)])*radius, dtype=int)
+def circle_at(center: Tuple[int, int], radius: int, angle_from_0: float64) -> Tuple[float64, Tuple[int, int]]:
+
+    coordinates = array(
+        array(center) + array([cos(angle_from_0), sin(angle_from_0)])*radius, dtype=int)
     ortho = pi/2 - angle_from_0
     return (ortho, (coordinates[0], coordinates[1]))
 
+
 def gabors_around_rec(width: int, height: int, amount_of_dots: int,
-                       offset: int, dot_size: int, gabor_freq: int,
-                       raidal_easing: int) -> List[Dot]:
+                      offset: int, dot_size: int, gabor_freq: int,
+                      raidal_easing: int) -> List[Dot]:
     length = 2*(width+height)
-    rects = [rec_at(i, width, height) for i in range(0, length, int(length/amount_of_dots))]
+    rects = [rec_at(i, width, height)
+             for i in range(0, length, int(length/amount_of_dots))]
     return [
         Dot(dot_size/2, array([x, y]) + offset, create_gabor_values(dot_size,
             rotation=rotation, frequency=gabor_freq, raidal_easing=raidal_easing))
         for (rotation, (x, y)) in rects]
 
-def gabors_around_circle(center: Tuple[int,int], radius: int, amount_of_dots: int, 
-                         dot_size: int, gabor_freq: int, radial_easing:int, flip_one=False) -> List[Dot]:
+
+def gabors_around_circle(center: Tuple[int, int], radius: int, amount_of_dots: int,
+                         dot_size: int, gabor_freq: int, radial_easing: int, flip_one=False) -> List[Dot]:
     angles = linspace(0, pi*2, amount_of_dots)
     properties = [circle_at(center, radius, angle) for angle in angles]
-    
-    flip_index = randint(0,len(angles)-1) if flip_one else -1
 
-    return [Dot(dot_size/2, array(position), 
+    flip_index = randint(0, len(angles)-1) if flip_one else -1
+
+    return [Dot(dot_size/2, array(position),
                 create_gabor_values(dot_size, gabor_freq, raidal_easing=radial_easing,
-                                     rotation=rotation+pi/2 if i == flip_index else rotation))
-                for (i,(rotation, position)) in enumerate(properties)]
+                                    rotation=rotation+pi/2 if i == flip_index else rotation))
+            for (i, (rotation, position)) in enumerate(properties)]
+
 
 def fill_with_dots(figure_size: int, dots_fill: List[NDArray], priority_dots: List[Dot] = []) -> NDArray:
     # there must be enough room for all the dots
@@ -259,6 +277,7 @@ def fill_with_dots(figure_size: int, dots_fill: List[NDArray], priority_dots: Li
         available_positions &= logical_not(circle <= square(dot.r*2))
 
     return canvas
+
 
 def dots_image(figure_size: int, dots_fill: List[NDArray], priority_dots: List[NDArray] = []) -> AppliablePixmap:
     return array_into_pixmap(
