659f4248b7afc52a56f1b3263cfefc4c46327261
Roei Shimron <roei@mail.academy.ac.il>
2025-03-13 13:48:04 +0000

    SSVEP contour integration experiment

--- experiments/area.py
+++ experiments/area.py
@@ -13,6 +13,10 @@ def flatten(lst):
     return [x for xs in lst for x in xs]
 
 
+def radius_into_amount_of_dots(r: int, coherence_at_full: int, full_radius: int) -> int:
+    return int(coherence_at_full*r/full_radius)
+
+
 def run():
     # Create the Qt Application
     app = QApplication(sys.argv)
@@ -21,36 +25,45 @@ def run():
 
     height = int(screen_height*2/3)
 
-    AMOUNT_OF_BASE = 60
+    AMOUNT_OF_BASE = 63
     DOT_SIZE = 70
-    COHERENCES = [10, 15, 20, 25]
-    DISPLAY_FREQUENCY = 5
-    TRIAL_DURATION = 15
-    AMOUNT_OF_EXAMPLES = 5
+    COHERENCES = [10, 12, 14, 16, 18, 20, 22]
+    DISPLAY_FREQUENCY = 6
+    TRIAL_DURATION = 16
+    AMOUNT_OF_EXAMPLES = 8
 
     RADIAL_EASING = 1200
     SPACIAL_FREQUENCY = 2
     AMOUNT_OF_TRIALS = len(COHERENCES)
-    
+    ODDBALL_RATIO = 2
+
+    MAX_RADIUS = int(height/2) - DOT_SIZE*2
+    MIN_RADIUS = int(MAX_RADIUS/2)
+
     AMOUNT_OF_DURATIONS = TRIAL_DURATION * DISPLAY_FREQUENCY * AMOUNT_OF_TRIALS
     assert AMOUNT_OF_DURATIONS == int(AMOUNT_OF_DURATIONS)
-
+    assert 60 % DISPLAY_FREQUENCY == 0
     durations = [1/DISPLAY_FREQUENCY*1000] * int(AMOUNT_OF_DURATIONS)
-    
-    assert (TRIAL_DURATION*DISPLAY_FREQUENCY) % (2*AMOUNT_OF_EXAMPLES) == 0
-    TRIAL_INFLATION = int((TRIAL_DURATION*DISPLAY_FREQUENCY)/2/AMOUNT_OF_EXAMPLES)
-    assert AMOUNT_OF_EXAMPLES*TRIAL_INFLATION*AMOUNT_OF_TRIALS*2 == AMOUNT_OF_DURATIONS
-    
+
+    assert (TRIAL_DURATION*DISPLAY_FREQUENCY) % (ODDBALL_RATIO *
+                                                 AMOUNT_OF_EXAMPLES) == 0
+    TRIAL_INFLATION = int(
+        (TRIAL_DURATION*DISPLAY_FREQUENCY)/ODDBALL_RATIO/AMOUNT_OF_EXAMPLES)
+    assert AMOUNT_OF_EXAMPLES*TRIAL_INFLATION * \
+        AMOUNT_OF_TRIALS*ODDBALL_RATIO == AMOUNT_OF_DURATIONS
+
     oddballs = [inflate_randomley([array_into_pixmap(
         fill_with_dots(int(height),
                        [create_gabor_values(
-                           DOT_SIZE, SPACIAL_FREQUENCY, rotation=random()*pi, offset=pi/2, raidal_easing=RADIAL_EASING)
-                           for _ in range(AMOUNT_OF_BASE - c)],
-                priority_dots=gabors_around_circle((int(height/2), int(height/2)),
-                                                   int(height/2) - DOT_SIZE,
-                                                   c, DOT_SIZE, SPACIAL_FREQUENCY,
+                           DOT_SIZE, SPACIAL_FREQUENCY, rotation=random()*pi,
+                           offset=pi/2, raidal_easing=RADIAL_EASING)
+                           for _ in range(AMOUNT_OF_BASE - radius_into_amount_of_dots(r, c, MAX_RADIUS))],
+                priority_dots=gabors_around_circle((int(height/2), int(height/2)), r,
+                                                   radius_into_amount_of_dots(
+                                                       r, c, MAX_RADIUS),
+                                                   DOT_SIZE, SPACIAL_FREQUENCY,
                                                    RADIAL_EASING, offset=random()*pi)))
-        for _ in range(AMOUNT_OF_EXAMPLES)], TRIAL_INFLATION)
+        for r in [randint(MIN_RADIUS, MAX_RADIUS) for _ in range(AMOUNT_OF_EXAMPLES)]], TRIAL_INFLATION)
         for c in COHERENCES]
 
     base = inflate_randomley([array_into_pixmap(
@@ -58,13 +71,13 @@ def run():
                        [create_gabor_values(
                            DOT_SIZE, SPACIAL_FREQUENCY, rotation=random()*pi, raidal_easing=RADIAL_EASING)
                            for _ in range(AMOUNT_OF_BASE)]))
-        for _ in range(AMOUNT_OF_EXAMPLES)], TRIAL_INFLATION*AMOUNT_OF_TRIALS)
-        
+        for _ in range(AMOUNT_OF_EXAMPLES*(ODDBALL_RATIO-1))], TRIAL_INFLATION*AMOUNT_OF_TRIALS)
+
     oddballs = flatten(oddballs)
     main_window = ViewExperiment.new(
-        OddballStimuli(height, iter(oddballs), iter(
-            base), 2), SoftSerial(), durations, True,
-        fixation="+")
+        OddballStimuli(height, cycle(oddballs), cycle(
+            base), ODDBALL_RATIO), SoftSerial(), durations, True,
+        fixation="+", allow_break=False)
 
     main_window.show()
 
