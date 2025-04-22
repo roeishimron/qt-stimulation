1cd693b244f400fbc10855a2c1f779e875fd41d4
Roei Shimron <roei@mail.academy.ac.il>
2025-04-22 10:15:43 +0000

    caching gabors for performence

--- experiments/noisy_circle/base.py
+++ experiments/noisy_circle/base.py
@@ -5,9 +5,10 @@ from soft_serial import SoftSerial
 from animator import OddballStimuli
 from itertools import cycle
 from viewing_experiment import ViewExperiment
-from random import random, randint
+from random import random, sample
 from numpy import array, pi, arange, square, average, inf, linspace
 from typing import Callable, Tuple, Iterable
+
 
 def flatten(lst):
     return [x for xs in lst for x in xs]
@@ -15,11 +16,6 @@ def flatten(lst):
 
 def radius_into_amount_of_dots(r: int, coherence_at_full: int, full_radius: int) -> int:
     return int(coherence_at_full*r/full_radius)
-
-# An experiment should contain:
-# 1. Behaviour, the subject with the patterned location of the circles should idenify the circles with less coherence
-# 2. Should be backed up with EEG signal increase at the identified coherence time
-# 3. Substract the difference between the pattern and non-patterned at the identified low coherence and receive the pattern+circle identification. Substract the circle identification from non-patterned higher coherence and receive the pattern identification
 
 
 def run(center_generaor: Callable[[int, int], Iterable[Tuple[int, int]]]):
@@ -55,30 +51,29 @@ def run(center_generaor: Callable[[int, int], Iterable[Tuple[int, int]]]):
     assert AMOUNT_OF_EXAMPLES*TRIAL_INFLATION * \
         AMOUNT_OF_TRIALS*ODDBALL_MODULATION == AMOUNT_OF_DURATIONS
 
+    # cache for performance
+    oriented_gabors = [create_gabor_values(DOT_SIZE, SPACIAL_FREQUENCY, rotation=r,
+                                           raidal_easing=RADIAL_EASING)
+                       for r in linspace(0, 2*pi, 1000)]
+    
+
     oddballs = [[array_into_pixmap(
-        fill_with_dots(int(height),
-                       [create_gabor_values(
-                           DOT_SIZE, SPACIAL_FREQUENCY, rotation=random()*pi,
-                           raidal_easing=RADIAL_EASING)
-                           for _ in range(AMOUNT_OF_BASE - coherence)],
+        fill_with_dots(int(height), sample(oriented_gabors, AMOUNT_OF_BASE - coherence),
                 priority_dots=gabors_around_circle(center, RADIUS, coherence,
                                                    DOT_SIZE, SPACIAL_FREQUENCY,
-                                                   RADIAL_EASING, offset=random()*pi)))
+                                                   RADIAL_EASING)))
                 for center in center_generaor(AMOUNT_OF_EXAMPLES, height)]*TRIAL_INFLATION
-        for coherence in COHERENCES]
+                for coherence in COHERENCES]
 
     base = inflate_randomley([array_into_pixmap(
         fill_with_dots(int(height),
-                       [create_gabor_values(
-                           DOT_SIZE, SPACIAL_FREQUENCY, rotation=random()*pi,
-                             raidal_easing=RADIAL_EASING)
-                           for _ in range(AMOUNT_OF_BASE)]))
+                       sample(oriented_gabors, AMOUNT_OF_BASE)))
         for _ in range(AMOUNT_OF_EXAMPLES*(ODDBALL_MODULATION-1))], TRIAL_INFLATION*AMOUNT_OF_TRIALS)
 
     oddballs = flatten(oddballs)
     main_window = ViewExperiment.new(
         OddballStimuli(height, cycle(oddballs), cycle(
-            base), ODDBALL_MODULATION), SoftSerial(), durations, False,
+            base), ODDBALL_MODULATION), SoftSerial(), durations, True,
         fixation="+", allow_break=False)
 
     main_window.show()
