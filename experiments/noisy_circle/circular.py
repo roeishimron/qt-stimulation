ee04759b17b7f57314ba9bdbb3faf31939047495
Roei Shimron <roei@mail.academy.ac.il>
2025-04-29 10:58:29 +0000

    add possible critic to circular

--- experiments/noisy_circle/circular.py
+++ experiments/noisy_circle/circular.py
@@ -2,6 +2,9 @@ from stims import circle_at
 from numpy import linspace, pi
 from experiments.noisy_circle.base import run as base_run
 
+# Note: A (very) good critic VS this experiment is that if V1 is simpley summing the input over time, 
+#       than the coherence motion "power" will not be highly effected
+#       whereas the random coherence "power" will be distributed over multiple locations
 def circular_center_generator(length: int, frame_size: int):
     motion_radius = int(frame_size/5)
     center = (int(frame_size/2), int(frame_size/2))
