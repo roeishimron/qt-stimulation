beef40fa99cc9fe2054d42c1868ad3e66dae3660
Roei Shimron <roei@mail.academy.ac.il>
2025-05-18 08:23:56 +0000

    support radial movement

--- experiments/noisy_circle/base.py
+++ experiments/noisy_circle/base.py
@@ -9,6 +9,7 @@ from numpy import array, pi, arange, square, average, inf, linspace
 from typing import Callable, Tuple, Iterable, List
 from realtime_experiment import RealtimeViewingExperiment
 
+DOT_SIZE = 70
 
 def flatten(lst):
     return [x for xs in lst for x in xs]
@@ -27,22 +28,19 @@ def run(center_generaor: Callable[[int, int], Iterable[Tuple[int, int]]]):
     height = int(screen_height*5/6)
 
     AMOUNT_OF_BASE = 100
-    DOT_SIZE = 70
-    COHERENCES = [11] #[8, 9, 10, 11, 12]
+    COHERENCES = arange(5) + 7
 
     FRAME_RATE = 60
-    STIMULI_DISPLAY_FREQUENCY = 30
+    STIMULI_DISPLAY_FREQUENCY = 60
     assert FRAME_RATE % STIMULI_DISPLAY_FREQUENCY == 0
 
-    ODDBALL_MODULATION = 3
-    TRIAL_DURATION = 15
-    AMOUNT_OF_EXAMPLES = 15
+    ODDBALL_MODULATION = 4
+    TRIAL_DURATION = 16
+    AMOUNT_OF_EXAMPLES = 30
 
     RADIAL_EASING = 1000
     SPACIAL_FREQUENCY = 2
     AMOUNT_OF_TRIALS = len(COHERENCES)
-
-    RADIUS = int((height/2 - DOT_SIZE*2)/2)
 
     AMOUNT_OF_DURATIONS = TRIAL_DURATION * STIMULI_DISPLAY_FREQUENCY * AMOUNT_OF_TRIALS
     assert AMOUNT_OF_DURATIONS == int(AMOUNT_OF_DURATIONS)
@@ -60,17 +58,19 @@ def run(center_generaor: Callable[[int, int], Iterable[Tuple[int, int]]]):
                        for r in linspace(0, 2*pi, 1000)]
 
     oddballs = [[array_into_pixmap(
-        fill_with_dots(int(height), sample(oriented_gabors, AMOUNT_OF_BASE - coherence),
-                priority_dots=gabors_around_circle(center, RADIUS, coherence,
+        fill_with_dots(int(height), sample(oriented_gabors, 
+                                           AMOUNT_OF_BASE - int(coherence*relative_increase)),
+                priority_dots=gabors_around_circle(center, radius, 
+                                                   int(coherence*relative_increase),
                                                    DOT_SIZE, SPACIAL_FREQUENCY,
                                                    RADIAL_EASING, offset=random()*pi*2)))
-                for center in center_generaor(AMOUNT_OF_EXAMPLES, height)]*TRIAL_INFLATION
+                for center, radius, relative_increase in center_generaor(AMOUNT_OF_EXAMPLES, height)]*TRIAL_INFLATION
                 for coherence in COHERENCES]
 
     base = inflate_randomley([array_into_pixmap(
         fill_with_dots(int(height),
                        sample(oriented_gabors, AMOUNT_OF_BASE)))
-        for _ in range(AMOUNT_OF_EXAMPLES*(ODDBALL_MODULATION-1))], TRIAL_INFLATION*AMOUNT_OF_TRIALS)
+        for _ in range(AMOUNT_OF_EXAMPLES)], TRIAL_INFLATION*AMOUNT_OF_TRIALS*(ODDBALL_MODULATION-1))
 
     oddballs = flatten(oddballs)
 
--- experiments/noisy_circle/radial.py
+++ experiments/noisy_circle/radial.py
@@ -1,4 +1,3 @@
-from stims import circle_at
 from numpy import linspace, pi
 from experiments.noisy_circle.base import run as base_run
 from experiments.noisy_circle.base import DOT_SIZE
@@ -7,10 +6,9 @@ from experiments.noisy_circle.base import DOT_SIZE
 #       than the coherence motion "power" will not be highly effected
 #       whereas the random coherence "power" will be distributed over multiple locations
 def circular_center_generator(length: int, frame_size: int):
-    motion_radius = int(frame_size/5)
     center = (int(frame_size/2), int(frame_size/2))
-    return ((circle_at(center, motion_radius, angle)[1] , int((frame_size/2 - DOT_SIZE*2)/2), 1)
-                               for angle in linspace(0, 2*pi, length, False))
+    base_radius = int((frame_size/2 - DOT_SIZE*2)/2)
+    return ((center , r, r/base_radius) for r in linspace(base_radius, base_radius*2, length))
 
 def run():
     return base_run(circular_center_generator)
