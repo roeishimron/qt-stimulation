cd4b9bf65a7e8628a8a5cb16d9d8e6336fb0e284
Roei Shimron <roei@mail.academy.ac.il>
2025-05-28 12:57:25 +0000

    words vs nons is now realtime

--- experiments/words_vs_nones.py
+++ experiments/words_vs_nones.py
@@ -5,6 +5,7 @@ from PySide6.QtGui import QFont
 from stims import inflate_randomley
 from soft_serial import SoftSerial
 from animator import OddballStimuli, AppliableText, DEFAULT_FONT
+from realtime_experiment import RealtimeViewingExperiment
 from itertools import cycle
 from viewing_experiment import ViewExperiment
 from random import choices, shuffle, randint, choice
@@ -23,7 +24,7 @@ SCRAMBELED = [scramble_str(w) for w in COMMON_HEBREW_WORDS]
 
 
 def create_appliable_text(t: str) -> AppliableText:
-    is_different_font = choices([True, False], [0.1, 0.9])[0]
+    is_different_font = False #choices([True, False], [0.1, 0.9])[0]
     if is_different_font:
         return AppliableText(t, font_family="Yehuda CLM", font_size=50)
     return AppliableText(t, font_size=50)
@@ -33,17 +34,13 @@ def run():
     # Create the Qt Application
     app = QApplication(sys.argv)
 
-    screen_height = app.primaryScreen().geometry().height()
-
-    size = int(screen_height*3/4)
-
     oddballs = map(create_appliable_text,
                   inflate_randomley(COMMON_HEBREW_WORDS, 10))
     stimuli = map(create_appliable_text, inflate_randomley(SCRAMBELED, 10))
 
-    main_window = ViewExperiment.new_with_constant_frequency(OddballStimuli(
-        cycle(oddballs), cycle(stimuli), 3), SoftSerial(), 3, trial_duration=45)
-    main_window.show()
+    main_window = RealtimeViewingExperiment(OddballStimuli(
+        cycle(oddballs), cycle(stimuli), 3), SoftSerial(), 10, 60/10*30, show_fixation_cross=False)
+    main_window.showFullScreen()
 
     # Run the main Qt loop
     app.exec()
