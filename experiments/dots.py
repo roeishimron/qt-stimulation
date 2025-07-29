2d1ee62d937cce27924657f48b93f4b89d136a04
Roei Shimron <roei@mail.academy.ac.il>
2025-07-29 12:52:47 +0000

    dots supporting realtime

--- experiments/dots.py
+++ experiments/dots.py
@@ -7,7 +7,20 @@ from itertools import cycle
 from viewing_experiment import ViewExperiment
 from random import choices
 from numpy import array, pi, arange, square, average
+from realtime_experiment import RealtimeViewingExperiment
 
+REFRESH_RATE = 60
+DISPLAY_RATE = 6
+assert REFRESH_RATE%DISPLAY_RATE == 0
+FRAMES_PER_STIM = int(REFRESH_RATE/DISPLAY_RATE)
+TRIAL_DURATION = 15
+AMOUNT_OF_STIMULI = TRIAL_DURATION * DISPLAY_RATE
+ODDBALL_MODULATION = 2
+
+assert AMOUNT_OF_STIMULI%ODDBALL_MODULATION == 0
+
+AMOUNT_OF_ODDBALLS = int(AMOUNT_OF_STIMULI/ODDBALL_MODULATION)
+AMOUNT_OF_BASE = AMOUNT_OF_STIMULI - AMOUNT_OF_ODDBALLS
 
 def run():
     # Create the Qt Application
@@ -18,38 +31,39 @@ def run():
     height = int(screen_height*2/3)
 
     # According to https://www.pnas.org/doi/epdf/10.1073/pnas.1917849117 we control ONLY the field because it is more important than controlling the figure size.
-    BASE_SIZE_RANGE = arange(50, 68)
-    ODDBALL_SIZE_RANGE = arange(58, 86)
 
-    AMOUNT_OF_BASE = 12
-    AMOUNT_OF_ODDBALL = 8
+    BASE_AMOUNT_OF_CIRCLES = 20
+    ODDBALL_AMOUNT_OF_CIRCLES = 40
 
-    surface_diff = average(pi*square(BASE_SIZE_RANGE))*AMOUNT_OF_BASE - \
-        average(pi*square(ODDBALL_SIZE_RANGE))*AMOUNT_OF_ODDBALL
+    BASE_SIZE_RANGE = arange(29, 43)
+    ODDBALL_SIZE_RANGE = arange(19, 32)
+
+    surface_diff = average(pi*square(BASE_SIZE_RANGE))*BASE_AMOUNT_OF_CIRCLES - \
+        average(pi*square(ODDBALL_SIZE_RANGE))*ODDBALL_AMOUNT_OF_CIRCLES
+    print(f"Surface diff is {surface_diff}")
     assert abs(surface_diff) < 100
 
-    oddball = (array_into_pixmap(
+    oddball = [array_into_pixmap(
         fill_with_dots(int(height),
                        [create_gabor_values(
-                           size, 0) - 1 for _ in range(int(AMOUNT_OF_ODDBALL))],
-                       backdround_value=1,
+                           size, 0) - 1 for _ in range(int(ODDBALL_AMOUNT_OF_CIRCLES))],
                        minimum_distance_factor=2))
-               for size in choices(ODDBALL_SIZE_RANGE, k=20))
+               for size in choices(ODDBALL_SIZE_RANGE, k=AMOUNT_OF_ODDBALLS)]
 
-    base = (array_into_pixmap(
+    base = [array_into_pixmap(
         fill_with_dots(int(height),
                        [create_gabor_values(
-                           size, 0) - 1 for _ in range(int(AMOUNT_OF_BASE))],
-                       backdround_value=1,
+                           size, 0) - 1 for _ in range(int(BASE_AMOUNT_OF_CIRCLES))],
                        minimum_distance_factor=2))
-            for size in choices(BASE_SIZE_RANGE, k=100))
-
-    main_window = ViewExperiment.new_with_constant_frequency(
-        OddballStimuli(cycle(inflate_randomley(oddball, 10)),
-                       cycle(inflate_randomley(base, 10)), 8), SoftSerial(), 10, True,
-        fixation="", trial_duration=45, background="white")
-
-    main_window.show()
+            for size in choices(BASE_SIZE_RANGE, k=AMOUNT_OF_BASE)]
+    
+    main_window = RealtimeViewingExperiment(OddballStimuli(iter(oddball), iter(base), ODDBALL_MODULATION),
+                                            SoftSerial(), 
+                                            FRAMES_PER_STIM, 
+                                            AMOUNT_OF_STIMULI,
+                                            show_fixation_cross=False)
+    
+    main_window.showFullScreen()
 
     # Run the main Qt loop
     app.exec()
