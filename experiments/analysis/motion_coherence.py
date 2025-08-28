251130d3f4ac20fb7dda5cb41461b421edb48108
Roei Shimron <roei@mail.academy.ac.il>
2025-08-28 10:07:49 +0000

    add comparative analysis

--- experiments/analysis/motion_coherence.py
+++ experiments/analysis/motion_coherence.py
@@ -1,30 +1,39 @@
 from re import search, findall
-from numpy import fromstring, array2string, array, float64, sort, log
-from matplotlib.pyplot import subplots, show
+from typing import Tuple
+from numpy import fromstring, array2string, array, float64, argsort, log
+from numpy.typing import NDArray
+from matplotlib.pyplot import legend, subplots, show
 import glob
 import os
 
 
-LOGFILE = "output/roei17-motion_coherence_roving-1756148886"
+FOLDER_PATH = 'output'
+LOGFILE = f"{FOLDER_PATH}/roei17-motion_coherence_roving-1756148886"
 
-def analyze_latest(latest=True):
 
-    folder_path = 'output'
-    list_of_files = glob.glob(os.path.join(folder_path, '*'))
+def analyze_latest():
+    list_of_files = glob.glob(os.path.join(FOLDER_PATH, '*'))
     latest_file = max(list_of_files, key=os.path.getctime)
 
-    text = open(latest_file if latest else LOGFILE, "r").read().replace("\n","")
+    subject_search = search(r"^(.*)-motion_coherence", latest_file)
+    assert subject_search is not None
+
+    analyze_subject(subject_search.group(1))
+
+
+# returns
+def text_into_coherences_and_successes(text: str) -> Tuple[NDArray, NDArray]:
     coherences = search(r"\[(.*)\] and direction", text)
     success = findall(r"(True|False)", text)
 
-
-    if coherences is None:
-        exit(1)
+    assert coherences is not None
 
     coherences = fromstring(coherences.group(1), dtype=float64, sep=" ")
     success = array([s == "True" for s in success])
 
-    averages = {c:0.0 for c in coherences}
+    assert len(coherences) == len(success)
+
+    averages = {c: 0.0 for c in coherences}
 
     assert len(coherences) % len(averages) == 0
     amount_of_repetitions = len(coherences) / len(averages)
@@ -33,12 +42,36 @@ def analyze_latest(latest=True):
         averages[c] += int(s) / amount_of_repetitions
 
     coherences, successes = zip(*averages.items())
-    coherences_and_successes = sort(array([coherences, successes]))
+    coherences, successes = array(coherences), array(successes)
+    sort_indices = argsort(coherences)
+    coherences, successes = coherences[sort_indices], successes[sort_indices]
+
+    return coherences, successes
 
 
-    _, ax = subplots()
-    ax.semilogx(coherences_and_successes[0], coherences_and_successes[1])
+def analyze_subject(subject_name: str):
+    list_of_files = glob.glob(os.path.join(FOLDER_PATH, f'{subject_name}-*'))
+    kinds = [search(r"(fixed|roving)", filename).group(1)
+             for filename in list_of_files]
+    times = [int(search(r"(\d*)$", filename).group(1))
+             for filename in list_of_files]
+    ordered = times[0] < times[1]
+
+    if not ordered:
+        list_of_files.reverse()
+        kinds.reverse()
+
+    assert len(list_of_files) > 0
+    _, ax = subplots(label=f"analysis of {subject_name}")
+
+    for filename, kind in zip(list_of_files, kinds):
+        coherences, successes = text_into_coherences_and_successes(
+            open(filename).read().replace("\n", ""))
+        ax.semilogx(coherences, successes, label=f"{kind}")
+
+    legend()
     show()
 
+
 if __name__ == "__main__":
-    analyze_latest(False)
+    analyze_subject("10461")
