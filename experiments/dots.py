130eb84cca8a05817644f75b1ae951dda851cd50
Roei Shimron <roei@mail.academy.ac.il>
2025-09-16 13:06:56 +0000

    add dots experiment

--- experiments/dots.py
+++ experiments/dots.py
@@ -1,69 +1,61 @@
+from experiments.constant_stimuli.dots_generator import generate_moving_dots
 import sys
+from PySide6.QtCore import QPointF
 from PySide6.QtWidgets import QApplication
-from stims import fill_with_dots, inflate_randomley, create_gabor_values, array_into_pixmap
 from soft_serial import SoftSerial
 from animator import OddballStimuli
-from itertools import cycle
-from viewing_experiment import ViewExperiment
-from random import choices
-from numpy import array, pi, arange, square, average
+from stims import fill_with_dots, array_into_pixmap, Dot
+from constant_stimuli_experiment import ConstantStimuli, DirectionValidator
+from numpy.random import random, uniform
+from numpy import pi, deg2rad, array2string, array, ones
+from experiments.analysis.motion_coherence import analyze_latest
 from realtime_experiment import RealtimeViewingExperiment
 
-REFRESH_RATE = 60
-DISPLAY_RATE = 6
-assert REFRESH_RATE%DISPLAY_RATE == 0
-FRAMES_PER_STIM = int(REFRESH_RATE/DISPLAY_RATE)
-TRIAL_DURATION = 15
-AMOUNT_OF_STIMULI = TRIAL_DURATION * DISPLAY_RATE
-ODDBALL_MODULATION = 2
+from logging import getLogger
+logger = getLogger(__name__)
 
-assert AMOUNT_OF_STIMULI%ODDBALL_MODULATION == 0
-
-AMOUNT_OF_ODDBALLS = int(AMOUNT_OF_STIMULI/ODDBALL_MODULATION)
-AMOUNT_OF_BASE = AMOUNT_OF_STIMULI - AMOUNT_OF_ODDBALLS
 
 def run():
+    
     # Create the Qt Application
     app = QApplication(sys.argv)
 
     screen_height = app.primaryScreen().geometry().height()
 
-    height = int(screen_height*2/3)
+    size = int(screen_height * 5 / 6)
 
-    # According to https://www.pnas.org/doi/epdf/10.1073/pnas.1917849117 we control ONLY the field because it is more important than controlling the figure size.
+    SCREEN_REFRESH_RATE = 60
+    TRIAL_DURATION = 10
+    STIMULI_REFRESH_RATE = 60
+    ODDBALL_MODULATION = 1
 
-    BASE_AMOUNT_OF_CIRCLES = 20
-    ODDBALL_AMOUNT_OF_CIRCLES = 40
+    AMOUNT_OF_STIMULI = TRIAL_DURATION * STIMULI_REFRESH_RATE
+    FRAMES_PER_STIM = int(SCREEN_REFRESH_RATE / STIMULI_REFRESH_RATE)
+    assert SCREEN_REFRESH_RATE % STIMULI_REFRESH_RATE == 0
+    assert AMOUNT_OF_STIMULI % ODDBALL_MODULATION == 0
 
-    BASE_SIZE_RANGE = arange(29, 43)
-    ODDBALL_SIZE_RANGE = arange(19, 32)
+    DOT_RADIUS = 20
+    AMOUNT_OF_DOTS = 50
+    VELOCITY = 12
+    MEAN_LIFETIME = AMOUNT_OF_STIMULI // 2
 
-    surface_diff = average(pi*square(BASE_SIZE_RANGE))*BASE_AMOUNT_OF_CIRCLES - \
-        average(pi*square(ODDBALL_SIZE_RANGE))*ODDBALL_AMOUNT_OF_CIRCLES
-    print(f"Surface diff is {surface_diff}")
-    assert abs(surface_diff) < 100
+    trial = generate_moving_dots(AMOUNT_OF_DOTS, DOT_RADIUS,
+                                   size, AMOUNT_OF_STIMULI,
+                                   array([[0, 0.33, 2], [pi/2, 0.33, 4]]), VELOCITY, MEAN_LIFETIME, 6)
 
-    oddball = [array_into_pixmap(
-        fill_with_dots(int(height),
-                       [create_gabor_values(
-                           size, 0) - 1 for _ in range(int(ODDBALL_AMOUNT_OF_CIRCLES))],
-                       minimum_distance_factor=2))
-               for size in choices(ODDBALL_SIZE_RANGE, k=AMOUNT_OF_ODDBALLS)]
+    frames = [array_into_pixmap(
+                                fill_with_dots(size, [], 
+                                               [Dot(int(d.r), 
+                                                    array([d.x, d.y], dtype=int),
+                                                    d.color * ones((2*d.r, 2*d.r))) for d in f],
+                                              0, 0))
+                for f in trial]
 
-    base = [array_into_pixmap(
-        fill_with_dots(int(height),
-                       [create_gabor_values(
-                           size, 0) - 1 for _ in range(int(BASE_AMOUNT_OF_CIRCLES))],
-                       minimum_distance_factor=2))
-            for size in choices(BASE_SIZE_RANGE, k=AMOUNT_OF_BASE)]
-    
-    main_window = RealtimeViewingExperiment(OddballStimuli(iter(oddball), iter(base), ODDBALL_MODULATION),
-                                            SoftSerial(), 
-                                            FRAMES_PER_STIM, 
-                                            AMOUNT_OF_STIMULI,
-                                            show_fixation_cross=False)
-    
-    main_window.showFullScreen()
 
+    experiment = RealtimeViewingExperiment(
+        OddballStimuli(frames), SoftSerial(), FRAMES_PER_STIM, AMOUNT_OF_STIMULI, use_step=True
+    )
+
+    experiment.showFullScreen()
     # Run the main Qt loop
     app.exec()
