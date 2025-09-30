a4b635b42d2ca9b591441aef3e5da26d5769fce5
Roei Shimron <roei@mail.academy.ac.il>
2025-09-30 07:49:00 +0000

    allowing choosing the order of fixed and roving and allow reruns

--- experiments/constant_stimuli/dots_trials.py
+++ experiments/constant_stimuli/dots_trials.py
@@ -1,3 +1,4 @@
+from itertools import chain
 from experiments.constant_stimuli.examples.first import run as first_example_run
 from experiments.constant_stimuli.examples.second import run as second_example_run
 from experiments.constant_stimuli.examples.third import run as third_example_run
@@ -10,20 +11,22 @@ from logging import basicConfig, INFO
 
 
 def run(subject_name: str = "test"):
-    examples = [(f"example-{i}", f) for i, f in enumerate(
-        [first_example_run, second_example_run, third_example_run])]
+    examples = ((f"example-{i}", f) for i, f in enumerate(
+        [first_example_run, second_example_run, third_example_run]))
     
-    actual_experiments = [("fixed", fixed_run), ("roving", roving_run)]
-    shuffle(actual_experiments)
+    actual_experiments = (("fixed", fixed_run), ("roving", roving_run))
+    print(f"Would you like to run FIXED first? (Y/n)")
+    if input() == "n":
+        actual_experiments = reversed(actual_experiments)
 
-    expeiments = examples + actual_experiments
 
-    for experiment_name, experiment in expeiments:
-        print(f"Would you like to run {experiment_name}? (Y/n)")
-        if input() == "n":
-            continue
+    for experiment_name, experiment in chain(examples, actual_experiments):
+        while True:
+            print(f"Would you like to (re)run {experiment_name}? (Y/n)")
+            if input() == "n":
+                break
         
-        logging_filename = f"./output/{subject_name}-{experiment_name}-{time_ns()//10**9}"
-        basicConfig(level=INFO, filename=logging_filename, force=True)
-        experiment()
+            logging_filename = f"./output/{subject_name}-{experiment_name}-{time_ns()//10**9}"
+            basicConfig(level=INFO, filename=logging_filename, force=True)
+            experiment()
 
